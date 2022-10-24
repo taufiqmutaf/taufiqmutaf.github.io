1dfaa463875aa50ec70fd1f8f1b9c2f2d19eab8f
Muhammad Taufiq <muhammadtaufiqr89@gmail.com>
2022-10-24 11:09:29 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "This is 911 !!! Whats your emergency ??<br>";
+
+    $nama = "Muhammad Taufiq";
+    $umur = 19;
+
+    echo "My Name is $nama, i'm $umur th years old<br>";
+
+    echo "Whats 15 x 12 ?<br>";
+    $number1 = 15;
+    $number2 = 12;
+
+    $hasil = $number1 * $number2;
+
+    echo "Absolutely $hasil !!";
+
+?>
